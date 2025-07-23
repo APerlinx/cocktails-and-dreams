839b90ed88e9cc107f0cbf63293453a15060da5d
APerlinx <133282938+APerlinx@users.noreply.github.com>
2025-07-23 14:17:54 +0300

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './_styles/globals.css'
 import { ModalProvider } from './context/MenuContext'
 import MenuModal from './_components/MenuModal'
 import { ReactNode } from 'react'
+import { Metadata } from 'next'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -14,9 +15,24 @@ const geistMono = Geist_Mono({
   subsets: ['latin'],
 })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Cocktails and dreams',
   description: 'Cocktail and dreams website',
+  openGraph: {
+    title: 'Cocktails and dreams',
+    description: 'Best cocktails in town',
+    url: 'https://cocktails-and-dreams.com',
+    siteName: 'Cocktails & Dreams',
+    images: [
+      {
+        url: '/logo.png',
+        width: 1200,
+        height: 630,
+        alt: 'Cocktail bar preview',
+      },
+    ],
+    type: 'website',
+  },
 }
 
 type RootLayoutProps = {
