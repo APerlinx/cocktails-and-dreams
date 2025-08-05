53ff18b398d920b2557c6baa5f2b6d9960009139
APerlinx <133282938+APerlinx@users.noreply.github.com>
2025-08-05 14:51:39 +0300

    Add And Fix: Text and replace icon

--- app/_components/MenuCircle.tsx
+++ app/_components/MenuCircle.tsx
@@ -3,30 +3,38 @@
 import Image from 'next/image'
 import { FC } from 'react'
 import { useModal } from '../context/MenuContext'
+import { Martini } from 'lucide-react'
 
 const MenuCircle: FC = () => {
   const { toggleMenu } = useModal()
 
   return (
-    <button
-      onClick={(e) => {
-        e.stopPropagation()
-        toggleMenu()
-      }}
-      className="fixed bottom-4 right-8 z-[100] hover:cursor-pointer"
-    >
-      <div className="relative w-12 h-12 rounded-full p-[2px] bg-black/80 border-2 border-gray-500">
-        <div className="w-full h-full rounded-full overflow-hidden hover:bg-gray-100 duration-300">
-          <Image
-            src="/logo-notext.svg"
-            alt="home page link"
-            width={12}
-            height={12}
-            className="object-cover w-full h-full p-2"
-          />
+    <div className="fixed bottom-4 right-2 flex flex-col items-center">
+      <svg width="100" height="30" className="-mb-4">
+        <path id="curveNav" d="M10,52 Q52,-30 90,55" fill="transparent" />
+        <text width="100%" textAnchor="middle">
+          <textPath
+            href="#curveNav"
+            startOffset="50%"
+            className="text-xs fill-gray-900"
+          >
+            navigation
+          </textPath>
+        </text>
+      </svg>
+
+      <button
+        onClick={(e) => {
+          e.stopPropagation()
+          toggleMenu()
+        }}
+        className="relative w-12 h-12 rounded-full p-[2px] bg-black/80 border-2 border-gray-500 flex items-center justify-center z-[100] hover:cursor-pointer"
+      >
+        <div className="w-full h-full rounded-full overflow-hidden hover:bg-gray-100 duration-300 flex items-center justify-center">
+          <Martini className="text-white/80 text-center object-cover w-full h-full p-2" />
         </div>
-      </div>
-    </button>
+      </button>
+    </div>
   )
 }
 
